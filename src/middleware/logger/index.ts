4c2efc0c0899cee060fb85bbbd253ab6ce03aa25
yibaebi <elliot.yibaebi@yahoo.com>
2023-05-06 23:56:57 +0100

    create logger middleware

---
+++ src/middleware/logger/index.ts
@@ -0,0 +1,42 @@
+import { format, createLogger, transports } from 'winston'
+const { combine, errors, timestamp, colorize } = format
+
+// Default log config
+const DIRNAME = './logs'
+
+const readableLogFormat = format.printf(
+  ({ timestamp, level, message, stack }) => {
+    return `${timestamp} ${level.toUpperCase()} ${stack || message}\n`
+  }
+)
+
+const DEFAULT_LOG_FORMAT = combine(
+  errors({ stack: false }),
+  timestamp(),
+  combine(colorize({ level: true })),
+  readableLogFormat
+)
+
+const GEN_CONFIG = {
+  dirname: DIRNAME,
+  extension: '.log',
+  zippedArchive: true
+}
+
+const logger = createLogger({
+  format: DEFAULT_LOG_FORMAT,
+  transports: [
+    new transports.Console({ level: 'info' }),
+    new transports.File({ filename: 'error', level: 'error', ...GEN_CONFIG })
+  ],
+  exceptionHandlers: [
+    new transports.Console(),
+    new transports.File({
+      filename: 'exceptions',
+      handleRejections: true,
+      ...GEN_CONFIG
+    })
+  ]
+})
+
+export { logger }
